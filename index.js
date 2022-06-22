16b1ba424cb5be4020fdc85c242f8f9379d76932
CheeryCodes <esthernehemiah57@gmail.com>
2022-06-22 22:26:44 +0100

    Simple Javascript Calculator

---
+++ index.js
@@ -0,0 +1,33 @@
+//basic calculator
+
+//taking input
+const operator = prompt("Enter the Operator you want to work with (either +,-,* or /): ");
+
+const num1 = parseFloat(prompt("Enter first number: "));
+
+const num2 = parseFloat(prompt("Enter second number: "));
+
+let result;
+if (isNaN(num1)  || isNaN(num2) ) {
+    alert("Enter Numbers only!");
+}
+else if (operator == '+'){
+    result = num1 + num2;
+}
+
+else if (operator == '-'){
+    result = num1 - num2;
+}
+
+else if (operator == '*'){
+    result = num1 * num2;
+}
+
+else if (operator == '/'){
+    result = num1 / num2;
+}
+else{
+    alert("Invalid Operator!");
+}
+
+alert("Your answer is: "+result);
